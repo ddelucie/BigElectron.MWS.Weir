591560a9c3017479161974e068d88049848a49ff
ddelucie <ddelucie@hotmail.com>
2018-08-23 22:30:11 -0700

    changed property style

--- BigElectron.MWS.Handlers/ServiceContext.cs
+++ BigElectron.MWS.Handlers/ServiceContext.cs
@@ -9,12 +9,29 @@ namespace BigElectron.MWS.Handlers
 		private string _applicationName = "BigElectron";
 		private string _mwsServiceUrl = "https://mws.amazonservices.com";
 
-		public string MwsServiceUrl { get => _mwsServiceUrl; set => _mwsServiceUrl = value; }
+		public string MwsServiceUrl
+		{
+			get { return _mwsServiceUrl; }
+			set { _mwsServiceUrl = value; }
+		}
 		public string AccessKey { get; set; }
 		public string SecretKey { get; set; }
-		public string ApplicationName { get => _applicationName; set => _applicationName = value; }
-		public string AppVersion { get => _appVersion; set => _appVersion = value; }
+		public string ApplicationName
+		{
+			get { return _applicationName; }
+			set { _applicationName = value; }
+		}
+		public string AppVersion
+		{
+			get { return _appVersion; }
+			set { _appVersion = value; }
+		}
 		public string SellerId { get; set; }
 		public string MwsAuthToken { get; set; }
+
+		private int myVar;
+
+
+
 	}
 }
